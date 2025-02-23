09f4064407a8afe4c3788979e90aba0a07827c5d
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-02-23 17:53:28 +0100

    feat(console): implement scrollup
    - scrollup when cursor out of screen
    - clean screen at init, and seak cursor to 0,0

--- include/n7OS/console.h
+++ include/n7OS/console.h
@@ -8,28 +8,28 @@
 
 #define SCREEN_ADDR 0xB8000
 
-#define PORT_CMD  0x3D4
+#define PORT_CMD 0x3D4
 #define PORT_DATA 0x3D5
 
-#define CMD_HIGH  0xE
-#define CMD_LOW   0xF
+#define CMD_HIGH 0xE
+#define CMD_LOW 0xF
 
-#define BLACK   0x0
-#define BLUE    0x1
-#define GREEN   0x2
-#define CYAN    0x3
-#define RED     0x4
-#define PURPLE  0x5
-#define BROWN   0x6
-#define GRAY    0x7
-#define D_GRAY  0x8
-#define L_BLUE  0x9
+#define BLACK 0x0
+#define BLUE 0x1
+#define GREEN 0x2
+#define CYAN 0x3
+#define RED 0x4
+#define PURPLE 0x5
+#define BROWN 0x6
+#define GRAY 0x7
+#define D_GRAY 0x8
+#define L_BLUE 0x9
 #define L_GREEN 0xA
-#define L_CYAN  0xB
-#define L_RED   0xC
+#define L_CYAN 0xB
+#define L_RED 0xC
 #define L_PURPLE 0xD
-#define YELLOW  0xE
-#define WHITE   0xF
+#define YELLOW 0xE
+#define WHITE 0xF
 
 // Character color: blink|back|text
 #define BLINK   (0<<7)
