23745db6303265494a7d8eae5707df04043f6080
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-02-28 10:58:53 +0100

    feat(mem): implement physical memory management

--- include/n7OS/mem.h
+++ include/n7OS/mem.h
@@ -10,52 +10,62 @@
 
 /**
  * @brief Adresse de la dernière ligne adressable de la mémoire
- * 
+ *
  * On considère ici une mémoire de 16Mo
  */
-#define LAST_MEMORY_INDEX 0XFFFFFF 
+#define LAST_MEMORY_INDEX 0xFFFFFF
 
 /**
  * @brief Taille d'une page en mémoire
- * 
+ *
  * Ici, 0x1000 -> 2^10 * 4 = 4096 octets
  */
 #define PAGE_SIZE 0x1000
 
 /**
+ * @brief Nombre de pages dans la mémoire
+ */
+#define PAGE_COUNT LAST_MEMORY_INDEX / PAGE_SIZE
+
+/**
+ * @brief Taille du tableau de bitmap contenant l'état des pages
+ */
+#define BITMAP_ARRAY_SIZE PAGE_COUNT / 32
+
+/**
  * @brief Marque la page allouée
- * 
+ *
  * Lorsque la page a été choisie, cette fonction permet de la marquer allouée
- * 
+ *
  * @param addr Adresse de la page à allouer
  */
 void setPage(uint32_t addr);
 
 /**
  * @brief Désalloue la page
- * 
+ *
  * Libère la page allouée.
- * 
+ *
  * @param addr Adresse de la page à libérer
  */
 void clearPage(uint32_t addr);
 
 /**
  * @brief Fourni la première page libre de la mémoire physique tout en l'allouant
- * 
+ *
  * @return uint32_t Adresse de la page sélectionnée
  */
 uint32_t findfreePage();
 
 /**
  * @brief Initialise le gestionnaire de mémoire physique
- * 
+ *
  */
 void init_mem();
 
 /**
  * @brief Affiche l'état de la mémoire physique
- * 
+ *
  */
 void print_mem();
 #endif
--- kernel/mem.c
+++ kernel/mem.c
@@ -1,50 +1,80 @@
 #include <n7OS/mem.h>
+#include <stdio.h>
+
+uint32_t page_bitmap_table[BITMAP_ARRAY_SIZE];
 
 /**
  * @brief Marque la page allouée
- * 
+ *
  * Lorsque la page a été choisie, cette fonction permet de la marquer allouée
- * 
+ *
  * @param addr Adresse de la page à allouer
  */
-void setPage(uint32_t addr) {
+void setPage(uint32_t addr)
+{
+    uint32_t offset = addr % 32;
+    uint32_t mask = 1 << offset;
 
+    page_bitmap_table[addr / 32] |= mask;
 }
 
 /**
  * @brief Désalloue la page
- * 
+ *
  * Libère la page allouée.
- * 
+ *
  * @param addr Adresse de la page à libérer
  */
-void clearPage(uint32_t addr) {
+void clearPage(uint32_t addr)
+{
+    uint32_t offset = addr % 32;
+    uint32_t mask = ~(1 << offset);
 
+    page_bitmap_table[addr / 32] &= mask;
 }
 
 /**
  * @brief Fourni la première page libre de la mémoire physique tout en l'allouant
- * 
+ *
  * @return uint32_t Adresse de la page sélectionnée
  */
-uint32_t findfreePage() {
-    uint32_t adresse= 0x0;
+uint32_t findfreePage()
+{
+    for (uint32_t addr = 0x0; addr < PAGE_COUNT; addr++)
+    {
+        uint32_t index = addr / PAGE_SIZE;
+        uint32_t offset = index % 32;
 
-    return adresse;
+        if (page_bitmap_table[index / 32] & (1 << offset))
+            return addr;
+    }
+
+    // an error occured, no free page found
+    return 0x0;
 }
 
 /**
  * @brief Initialise le gestionnaire de mémoire physique
- * 
  */
-void init_mem() {
-
+void init_mem()
+{
+    for (int i = 0x0; i < BITMAP_ARRAY_SIZE; i++)
+    {
+        page_bitmap_table[i] = 0x0;
+    }
 }
 
 /**
  * @brief Affiche l'état de la mémoire physique
- * 
+ *
  */
-void print_mem() {
-    
+void print_mem()
+{
+    for (int i = 0; i < BITMAP_ARRAY_SIZE; i++)
+    {
+        printf("0x%08x ", page_bitmap_table[i]);
+
+        if ((i + 1) % 7 == 0)
+            printf("\n");
+    }
 }
