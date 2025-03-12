ade7f7ed6270403779b7cb21ebd85eade27828fd
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-03-12 09:57:57 +0100

    feat(mem): implement paging

--- include/n7OS/paging.h
+++ include/n7OS/paging.h
@@ -8,38 +8,88 @@
 #include <inttypes.h>
 
 /**
- * @brief Description d'une ligne de la table de page
- * 
+ * @brief La page est présente en mémoire
  */
-typedef struct {
-    // a completer
-} page_table_entry_t;
+#define PRESENT 0x1
+
+/**
+ * @brief La page est accessible en écriture
+ */
+#define WRITABLE 0x1
+
+/**
+ * @brief La page est en mode utilisateur
+ */
+#define USERLAND 0x1
+
+/**
+ * @brief Une entrée dans le répertoire de page peut être manipulée en utilisant
+ *        la structure ou directement la valeur
+ */
+typedef union
+{
+    struct
+    {
+        uint32_t addr : 20;
+        uint16_t RSVD : 9;
+        uint8_t SU : 1;
+        uint8_t RW : 1;
+        uint8_t P : 1;
+    };
+    uint32_t value;
+} PDE; // PTE = Page Table Entry
+
+/**
+ * @brief Un répertoire de page (PageDirectory) est un tableau de descripteurs de page
+ */
+typedef PDE *PageDirectory;
 
 /**
  * @brief Une entrée dans la table de page peut être manipulée en utilisant
- *        la structure page_table_entry_t ou directement la valeur
+ *        la structure ou directement la valeur
  */
-typedef union {
-    page_table_entry_t page_entry;
+typedef union
+{
+    struct
+    {
+        uint32_t addr : 20;
+        uint8_t AVL : 3;
+        uint8_t RSVD2 : 2;
+        uint8_t D : 1;
+        uint8_t A : 1;
+        uint8_t RSVD1 : 2;
+        uint8_t SU : 1;
+        uint8_t RW : 1;
+        uint8_t P : 1;
+    };
     uint32_t value;
-} PTE; // PTE = Page Table Entry 
+} PTE; // PTE = Page Table Entry
 
 /**
  * @brief Une table de page (PageTable) est un tableau de descripteurs de page
- * 
  */
-typedef PTE * PageTable;
+typedef PTE *PageTable;
+
+typedef union
+{
+    struct
+    {
+        uint16_t page_idx : 12;
+        uint16_t table_idx : 10;
+        uint16_t directory_idx : 10;
+    };
+    uint32_t value;
+} Virtual_addr;
 
 /**
  * @brief Cette fonction initialise le répertoire de page, alloue les pages de table du noyau
  *        et active la pagination
- * 
  */
 void initialise_paging();
 
 /**
  * @brief Cette fonction alloue une page de la mémoire physique à une adresse de la mémoire virtuelle
- * 
+ *
  * @param address       Adresse de la mémoire virtuelle à mapper
  * @param is_writeable  Si is_writeable == 1, la page est accessible en écriture
  * @param is_kernel     Si is_kernel == 1, la page ne peut être accédée que par le noyau
--- kernel/kheap.c
+++ kernel/kheap.c
@@ -2,38 +2,55 @@
 
 /**
  * @brief Lorsque tout le code du noyau est compilé, le tas du noyau commence juste après.
- * 
+ *
  */
 extern uint32_t mem_heap;
-uint32_t placement_address = (uint32_t)&mem_heap;
+uint32_t placement_address;
 
-uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys) {
+void init_kheap()
+{
+    placement_address = (uint32_t)&mem_heap;
+}
+
+uint32_t get_stack_top()
+{
+    return placement_address;
+}
+
+uint32_t kmalloc_int(uint32_t sz, int align, uint32_t *phys)
+{
     uint32_t address;
-    if (align == 1 && (placement_address & 0xFFF) ) {
+    if (align == 1 && (placement_address & 0xFFF))
+    {
         // Aligne l'adresse si on n'est pas au début d'une page
         placement_address &= 0xFFFFF000;
         placement_address += 0x1000;
     }
-    if (phys) {
+    if (phys)
+    {
         *phys = placement_address;
     }
-    address= placement_address;
+    address = placement_address;
     placement_address += sz;
     return address;
 }
 
-uint32_t kmalloc_a(uint32_t sz) {
+uint32_t kmalloc_a(uint32_t sz)
+{
     return kmalloc_int(sz, 1, 0);
 }
 
-uint32_t kmalloc_p(uint32_t sz, uint32_t *phys) {
+uint32_t kmalloc_p(uint32_t sz, uint32_t *phys)
+{
     return kmalloc_int(sz, 0, phys);
 }
 
-uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys) {
+uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys)
+{
     return kmalloc_int(sz, 1, phys);
 }
 
-uint32_t kmalloc(uint32_t sz) {
+uint32_t kmalloc(uint32_t sz)
+{
     return kmalloc_int(sz, 0, 0);
 }
