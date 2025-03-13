c53d1a1db28337551d9347bb2fa3ea83021d8ebf
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-03-13 10:32:53 +0100

    fix(mem): correctly activate paging + fix structs + fix typos

--- kernel/paging.c
+++ kernel/paging.c
@@ -3,6 +3,7 @@
 #include <n7OS/mem.h>
 #include <n7OS/processor_structs.h>
 #include <stddef.h> // nécessaire pour NULL
+#include <stdio.h>
 
 PageDirectory directory;
 
@@ -13,7 +14,7 @@ void initialise_paging()
 
     for (int idx = 0; idx < PAGE_SIZE / sizeof(PDE); idx++)
     {
-        directory[idx].addr = kmalloc_a(PAGE_SIZE) >> 12;
+        directory[idx].page = kmalloc_a(PAGE_SIZE) >> 12;
         directory[idx].P = PRESENT;
         directory[idx].RW = WRITABLE;
         directory[idx].SU = USERLAND;
@@ -24,15 +25,15 @@ void initialise_paging()
         alloc_page_entry(i, 1, 1);
     }
 
-    __asm__ __volatile__("mov %0, %%cr3" ::"r"(&directory)); // set %cr3 with the directory addr
+    __asm__ __volatile__("mov %0, %%cr3" ::"r"(directory)); // set %cr3 with the directory addr
 
     // On active la pagination
     uint32_t cr0;
-    __asm__ __volatile__("mov %0, %%cr0" : "=r"(cr0));
+    __asm__ __volatile__("mov %%cr0, %0" : "=r"(cr0));
     cr0 |= 0x80000000;
-    __asm__ __volatile__("mov %%cr0, %0" ::"r"(cr0));
+    __asm__ __volatile__("mov %0, %%cr0" ::"r"(cr0));
 
-    // setup_base((uint32_t)directory);
+    setup_base((uint32_t)directory);
 }
 
 PageTable alloc_page_entry(uint32_t address, int is_writeable, int is_kernel)
@@ -41,13 +42,13 @@ PageTable alloc_page_entry(uint32_t address, int is_writeable, int is_kernel)
     virtual_addr.value = address;
 
     PDE pde = directory[virtual_addr.directory_idx];
-    PageTable pt = (PageTable)(pde.addr << 12);
-    PTE pte = pt[virtual_addr.table_idx];
+    PageTable pt = (PageTable)(pde.page << 12);
+    PTE *pte = &pt[virtual_addr.table_idx];
 
-    pte.P = PRESENT;
-    pte.RW = is_writeable;
-    pte.SU = !is_writeable;
-    pte.addr = findfreePage() >> 12;
+    pte->P = PRESENT;
+    pte->RW = is_writeable;
+    pte->SU = ~is_kernel;
+    pte->page = findfreePage() >> 12;
 
     return pt;
 }
