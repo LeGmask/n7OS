b3c8796c9b4d72eafcf1aa509952a9cfabb1f2db
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-03-19 16:23:36 +0100

    feat(timer): sync with RTC at init

--- kernel/timer.c
+++ kernel/timer.c
@@ -4,8 +4,29 @@
 
 uint32_t timestamp = 0;
 
+void rtc_sync()
+{
+	outb(0x0, 0x70);
+	unsigned char second = inb(0x71);
+	outb(0x2, 0x70);
+	unsigned char minute = inb(0x71);
+	outb(0x4, 0x70);
+	unsigned char hour = inb(0x71);
+
+	// BCD to binary conversion
+	second = (second & 0x0F) + ((second / 16) * 10);
+	minute = (minute & 0x0F) + ((minute / 16) * 10);
+	hour = (hour & 0x0F) + ((hour / 16) * 10);
+
+	timestamp = (hour * 3600 + minute * 60 + second) * 1000;
+	update_clock(timestamp / 1000); // Update clock
+}
+
 void init_timer()
 {
+	// Synchronize timestamp with RTC
+	rtc_sync();
+
 	// Configure PIT
 	outb(0x34, 0x43);
 	outb(FREQ & 0xFF, 0x40);
