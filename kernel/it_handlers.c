edd9b140ec66a46c4dc656a11a9ec0df3ad09f60
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-03-19 14:00:14 +0100

    feat(timer): support for timer and tick IT

--- kernel/it_handlers.c
+++ kernel/it_handlers.c
@@ -1,11 +1,16 @@
-#include <inttypes.h>
+#include <n7OS/it_handlers.h>
 #include <n7OS/irq.h>
+#include <n7OS/cpu.h>
+#include <n7OS/timer.h>
+#include <inttypes.h>
 #include <stdio.h>
 
+extern void handler_IT_32();
 extern void handler_IT_50();
 
-void init_IT_50()
+void init_IT()
 {
+	init_irq_entry(32, (uint32_t)handler_IT_32);
 	init_irq_entry(50, (uint32_t)handler_IT_50);
 }
 
