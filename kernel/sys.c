1b7450de5e44ae7f836a6f17ac562ad61d12a19f
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2025-04-04 15:42:58 +0200

    feat: syscalls support

--- kernel/sys.c
+++ kernel/sys.c
@@ -1,3 +1,4 @@
+#include <n7OS/cpu.h>
 #include <n7OS/sys.h>
 #include <n7OS/syscall_defs.h>
 #include <n7OS/console.h>
@@ -10,6 +11,8 @@ void init_syscall()
 {
 	// ajout de la fonction de traitement de l'appel systeme
 	add_syscall(NR_example, sys_example);
+	add_syscall(NR_shutdown, sys_shutdown);
+	add_syscall(NR_write, sys_write);
 
 	// initialisation de l'IT soft qui gère les appels systeme
 	init_irq_entry(0x80, (uint32_t)handler_syscall);
@@ -19,5 +22,22 @@ void init_syscall()
 int sys_example()
 {
 	// on ne fait que retourner 1
+	return 1;
+}
+
+int sys_shutdown(int n)
+{
+	if (n == 1)
+	{
+		outw(0x2000, 0x604); // Poweroff qemu > 2.0
+		return -1;
+	}
+	else
+		return n;
+}
+
+int sys_write(const char *s, int len)
+{
+	console_putbytes(s, len);
 	return 1;
 }
